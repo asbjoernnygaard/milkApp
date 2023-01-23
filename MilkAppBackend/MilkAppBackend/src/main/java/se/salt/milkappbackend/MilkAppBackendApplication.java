70527fe542ee3df33643392614c71b784c1aec37
Asbjørn Samuelsen Nygaard <asbjoern.nygaard@gmail.com>
2023-01-23 09:03:19 +0100

    initial committ

---
+++ MilkAppBackend/MilkAppBackend/src/main/java/se/salt/milkappbackend/MilkAppBackendApplication.java
@@ -0,0 +1,13 @@
+package se.salt.milkappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkAppBackendApplication.class, args);
+    }
+
+}

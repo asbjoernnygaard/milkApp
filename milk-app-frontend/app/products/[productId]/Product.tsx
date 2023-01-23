993c56a7a186d2159de0ff4933e0c3de54c27ff7
Asbjørn Samuelsen Nygaard <asbjoern.nygaard@gmail.com>
2023-01-23 22:00:38 +0100

    product list works

---
+++ milk-app-frontend/app/products/[productId]/Product.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = () => {
+  // if there is already product data for this product, use it. Else, fetch
+  return (
+      <div>
+        
+      </div>
+  );
+};
+
+export default Product;
